dec59cc81f24d8147cae2df20ddb862bcc2f8149
Eléna <elena.graf@bbz-cfp.ch>
2024-09-17 09:54:08 +0000

    Aufbau, Home-Seite, Nav, Footer, Neu-Seite

--- index.js
+++ index.js
@@ -10,14 +10,26 @@ const app = createApp({
 
 /* Startseite */
 app.get("/", async function (req, res) {
-  res.render("start", {});
+  res.render("beitraege", {});
+});
+
+app.get("/rangliste", async function (req, res) {
+  res.render("rangliste", {});
+});
+
+app.get("/neu", async function (req, res) {
+  res.render("neu", {});
+});
+
+app.get("/profil", async function (req, res) {
+  res.render("profil", {});
 });
 
 app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
 
-/* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
+/*Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
   console.log(`Example app listening at http://localhost:3010`);
 });
